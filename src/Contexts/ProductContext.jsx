543e4118831587f376e2f08cae65e5bc9a54c910
omid ghanbaran <omid.ghanbaran@gmail.com>
2023-05-25 20:24:48 +0330

    added tailwind

--- src/Contexts/ProductContext.jsx
+++ src/Contexts/ProductContext.jsx
@@ -2,19 +2,19 @@ import { createContext, useEffect, useState } from "react";
 
 export const ProductContext = createContext();
 
+// eslint-disable-next-line react/prop-types
 const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState();
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
-      console.log(data);
       setProducts(data);
     };
     fetchProducts();
   }, []);
   return (
-    <ProductContext.Provider value={products}>
+    <ProductContext.Provider value={{ products }}>
       {children}
     </ProductContext.Provider>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,26 @@
+import { useContext } from "react";
+import { ProductContext } from "../Contexts/ProductContext";
+
 const Home = () => {
-  return <div>Home</div>;
+  const { products } = useContext(ProductContext);
+  console.log(products);
+  return (
+    <div>
+      <section className="py-16">
+        <div className="container mx-auto">
+          <div className="grid">
+            {products.map((product) => {
+              return (
+                <div className="w-full h-[300px] bg-pink-200" key={product.id}>
+                  {product.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
